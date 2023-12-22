e9aebc1a17e1d2edf86f1e793f8ce54cb852873a
Filippo2803 <filippoborelli8@gmail.com>
2023-12-22 16:57:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const changeColor = document.addEventListener("scroll", () => {
+    let header = document.querySelector("header");
+    let getStartedBtn = document.getElementById("getStarted");
+    let scrollPosition = window.scrollY;
+    if(scrollPosition > 250){
+        header.style.backgroundColor = "#FFFFFF";
+        getStartedBtn.style.backgroundColor = "#1a8917";
+    } else {
+        header.style.backgroundColor = "#ffc017";
+        getStartedBtn.style.backgroundColor = "1a8917";
+    }
+});
